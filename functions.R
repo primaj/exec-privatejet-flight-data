3518c313d085dc4a212447c1abc1e6060f2a1790
John Primavesi <johnprimavesi@gmail.com>
2020-06-09 19:23:29 +0100

    write output file

--- functions.R
+++ functions.R
@@ -1,4 +1,6 @@
-scrape_dataset <- function(url){
+scrape_data <- function(url){
+  
+  on.exit(message('Scrape complete!\n'))
   
   url %>%
     read_html() %>% 
@@ -15,4 +17,32 @@ scrape_dataset <- function(url){
       epoch = as.numeric(substr(departure_time, 1, 10)),
       departure_time = as.Date(as.POSIXct(epoch, origin="1970-01-01"))
     )
+  
+  
+}
+
+prepare_data <- function(data){
+  
+  on.exit(message('Data prep. complete!\n'))
+  data %>% 
+    group_by(company, departure_time) %>% 
+    summarise(daily_count = n()) %>% 
+    ungroup() %>% 
+    pivot_wider(
+      id_cols = company,
+      values_from = daily_count,
+      names_from = departure_time
+    ) %>% 
+    pivot_longer(
+      cols = -company,
+      names_to = 'departure_time', 
+      values_to = 'daily_count'
+    ) %>% 
+    mutate_all(~replace_na(.x, 0)) %>% 
+    group_by(company) %>% 
+    arrange(departure_time) %>% 
+    mutate(daily_cum_total = cumsum(daily_count))
+  
+  
+  
 }
--- scrape_and_prep.R
+++ scrape_and_prep.R
@@ -4,5 +4,14 @@ library(tidyverse)
 source('config.R')
 source('functions.R')
 
-flight_data <- scrape_data(DATASET_URL)
+flight_data <- DATASET_URL %>% 
+  scrape_data() %>% 
+  prepare_data()
+  
+write_csv(flight_data, 'cumulative_company_flight_data.csv')
+
+  
+
+
+
 
